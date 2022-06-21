01a83886e1c7391f7f4ca8a52f0a0e3216242ea6
bmwmtv <bmwmtv@gmail.com>
2022-06-21 18:28:36 +0700

    fix

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
 
-import even from '../src/games/even.js';
+import startGame from '../src/games/even.js';
 
-even();
+startGame();
--- bin/brain-progression.js
+++ bin/brain-progression.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
 
-import progres from '../src/games/progression.js';
+import startGame from '../src/games/progression.js';
 
-progres();
+startGame();
